f3eefb33a4f5609ae637677bb69dd1ab969c2044
Ryan Emmans <ryanemmans@gmail.com>
2021-11-10 21:01:48 -0800

    Adds initial commit with all lambda functions.

---
+++ get-person/index.js
@@ -0,0 +1,31 @@
+const dynamoose = require('dynamoose');
+
+exports.handler = async (event) => {
+
+  const peopleSchema = new dynamoose.Schema({
+    'id': Number,
+    'name': String,
+    'role': String,
+  });
+
+  const peopleTable = dynamoose.model('people', peopleSchema);
+
+  let data = null;
+  let status = 500;
+
+  try {
+    let id = Number(event.pathParameters.id);
+    data = await peopleTable.query('id').eq(id).exec();
+    status = 200;
+
+  } catch (e) {
+    data = new Error(e);
+    status = 400;
+  }
+
+  const response = {
+    statusCode: status,
+    body: JSON.stringify(data),
+  };
+  return response;
+};
